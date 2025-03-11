e20349847b302ba2f7924f14a3118956b9543e89
TimNguyen21 <tim.nguyen2112@gmail.com>
2025-03-11 04:53:49 -0600

    [main] Initial setup of Redux with examples

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+// Redux Example
+import { updateDescription, updateDescriptionToDefault } from './redux/feature/example/exampleSlice';
+
 import './App.scss';
 
 function App() {
+  const example = useSelector((state) => state.example.description);
+  const dispatch = useDispatch();
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        {/*Redux example setup */}
+          {example}
+          <button onClick={() => dispatch(updateDescription('hi'))}>Update Message</button>
+          <button onClick={() => dispatch(updateDescriptionToDefault())}>Default Message</button>
+        {/*Redux example setup */}
       </header>
     </div>
   );
